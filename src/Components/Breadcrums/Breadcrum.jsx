e146c72058a4418ed16254acdd985a2d15816e26
venkata naga murali <nagamuralik9@gmail.com>
2023-12-19 16:03:11 +0530

    uploaded files

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "./Breadcrum.css";
-import arrow_icon from "../Assets/breadcrum_arrow.png";
-const Breadcrum = (props) => {
-  const { product } = props;
-  return (
-    <div className="breadcrum">
-      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />
-      {product.category} <img src={arrow_icon} alt="" /> {product.name}
-    </div>
-  );
-};
+import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png';
 
-export default Breadcrum;
+const Breadcrum = (props) =>{
+    const {product} = props
+    
+    return(
+
+        <div className="breadcrum">
+            Home <img src = {arrow_icon} alt="" /> SHOP <image src={arrow_icon} alt=""/> {product.category} 
+             <img src={arrow_icon} alt="" />{product.name}
+        </div>
+    )
+}
+
+export default Breadcrum
